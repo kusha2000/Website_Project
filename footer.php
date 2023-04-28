417314a3632c800cbe387d52a520cc2c995f5e5f
kusha2000 <kushanandarawewa1@gmail.com>
2023-04-28 11:53:04 +0530

    Create footer part of the web site

---
+++ footer.php
@@ -0,0 +1,37 @@
+<footer class="footer">
+    <section class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php"><img src="images/home.png">home</a>
+            <a href="shop.php"><img src="images/shop.png">Shop</a>
+            <a href="#"><img src="images/about.png">About Us</a>
+            <a href="#"><img src="images/contact.png">Contact Us</a>
+        </div>
+
+        <div class="box">
+            <h3>extra link</h3>
+            <a href="#"><img src="images/signin.png">Sign In</a>
+            <a href="#"><img src="images/signup.png">Sign Up</a>
+        </div>
+
+        <div class="box">
+            <h3>Contact info</h3>
+            <a href="#"><img src="images/phone.png">+94 1111111</a>
+            <a href="#"><img src="images/phone.png">+94 2222222</a>
+            <a href="#"><img src="images/envelope.png">abc@gmail.ccom</a>
+            <a href="#"><img src="images/market.png">Colombo</a>
+        </div>
+
+        <div class="box">
+            <h3>Follow us</h3>
+            <a href="#"><img src="images/facebook.png">facebook</a>
+            <a href="#"><img src="images/twitter.png">twitter</a>
+            <a href="#"><img src="images/instagram.png">instagram</a>
+            <a href="#"><img src="images/youtube.png">youtube</a>
+        </div>
+
+    </section>
+
+    <div class="credit">&copy; copyright @ 2023 by <span>Group 2</span> | all right reserved! </div>
+</footer>
--- home.php
+++ home.php
@@ -5,15 +5,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Phone Shop</title>
+
     <!-- css file -->
     <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
+    <!-- Header File -->
     <?php include 'header.php';?>
 
 
 
+
+    <!-- Footer File -->
+    <?php include 'footer.php'?>
+
+    <!-- JavaScript File -->
     <script src="js/script.js"></script>
 
 </body>
